ae49540c7d554086917504257ca213c36e54e38f
JivkoKelchev <jivko.r.kelchev@gmail.com>
2023-06-03 14:38:56 +0300

    Initial commit

---
+++ contracts/LimePlaceNFT.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract LimePlaceNFT is ERC721URIStorage{
+    using Counters for Counters.Counter;
+
+    Counters.Counter public tokenCount;
+
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+
+    function mint(string memory _tokenURI) external returns (uint256) {
+        tokenCount.increment();
+
+        _safeMint(msg.sender, tokenCount.current());
+        _setTokenURI(tokenCount.current(), _tokenURI);
+
+        return tokenCount.current();
+    }
+}
